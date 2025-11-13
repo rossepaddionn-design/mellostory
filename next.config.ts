370a74f78cef591a36b8eacddf8b5c037b1e4af4
rossepaddionn-design <rossepaddionn@gmail.com>
2025-11-13 19:58:29 +0300

    image fixs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'ngjjhsuifcldorbzmdj.supabase.co',
+      'ik.imagekit.io'
+    ],
+  },
 };
 
 export default nextConfig;
