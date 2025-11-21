73c620efb8c6791fe834c0aa2d3e3cc52921aa40
rossepaddionn-design <rossepaddionn@gmail.com>
2025-11-21 15:00:07 +0300

    new iconic

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MelloStory",
   description: "MelloStory novel archive",
-  icons: {
-    icon: '/favicon.jpg',
-  },
+icons: {
+  icon: '/favicon.ico',
+},
 };
 
 export default function RootLayout({
